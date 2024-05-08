37a6e79029c586b8eec15882448460a257d67faa
Botan Guzel <botanguzel@hotmail.com>
2024-05-08 08:02:52 +0000

    Plot the data from TextBlob.csv

---
+++ Graph_textblob.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+df=pd.read_csv("sentiment_analysis_results_tb.csv")
+top_flags = df['flags'].value_counts().head(20)
+
+df_top_flags = df[df['flags'].isin(top_flags.index)]
+
+sentiment_counts = df_top_flags.groupby(['flags', 'sentiment']).size().unstack(fill_value=0)
+sentiment_counts.drop(columns="Positive",inplace=True)
+plt.figure(figsize=(12, 8))
+sentiment_counts.plot(kind='bar', stacked=False)
+
+plt.title('Sentiment Distribution Across Top 20 Flags - TextBlob')
+plt.xlabel('Flags')
+plt.ylabel('Count')
+plt.xticks(rotation=45)
+
+plt.legend(title='Sentiment')
+plt.tight_layout()
+plt.show()
+#plt.savefig("tb_figure.png")
